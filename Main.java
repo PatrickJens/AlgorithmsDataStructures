4a55a9013648b7af1bd59670c78dc7ff532bbb2f
Patrick C <pcannell14@gmail.com>
2023-06-16 16:57:04 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import Sorts.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        int[] arr = AM.createIntArray(6);
+        int[] arr = AM.createIntArray(10);
         System.out.println(Arrays.toString(arr));
-        InsertionSort.sort(arr);
+        QuickSortLomuto.sort(arr);
         System.out.println("my sort=   " + Arrays.toString(arr));
         Arrays.sort(arr);
         System.out.println("java sort= " + Arrays.toString(arr));
